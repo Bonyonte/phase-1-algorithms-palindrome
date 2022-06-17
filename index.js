0f3233e5fdd292e223775354ee930b76034e2ad4
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-17 16:11:53 +0300

    commit tree 12e8d72365a4b57296dab93527610eb81cf3fd80

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+
+// function output(word){
+//   return word.split(" ").reverse().join(" ");
 function isPalindrome(word) {
+  let wordSplit = word.split("")
+  let outputWord = [];
+  wordSplit.map((word) => {
+    outputWord.unshift(word);
+  });
+  if( word === outputWord.join("")){
+    return true;
+  }
+  else{
+    return false;
+  }
   // Write your algorithm here
+ // 1. Write a function isPalindrome
+ // 2. Assign a parameter 
+ // 3. That function will reseive an argument and a string
+ // 4. The function returns true if the string is a palindrome
 }
 
 /* 
   Add your pseudocode here
+  initialize a function called isPalindrome
+
+  iterate over the string provided
+  if the sting isPalindrome;
+  return true
+  else
+  return false
+
 */
 
 /*
@@ -13,6 +39,8 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +48,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  // console.log("");
+
+  // console.log("Expecting: false");
+  // console.log("=>", isPalindrome("ab"));
+
+  // console.log("");
+
+  // console.log("Expecting: false");
+  // console.log("=>", isPalindrome("robot"));
+
+  // console.log("");
 }
 
 module.exports = isPalindrome;
